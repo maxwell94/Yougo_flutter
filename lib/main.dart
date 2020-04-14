e92de1375fa372be3cfde9413bcdccb266244386
Maxio225 <maxio225@MacBook-Air-di-Maxio225.local>
2020-04-15 01:01:50 +0200

    profile done

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:drawer/profile.dart';
 import 'package:drawer/reservation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -29,6 +31,25 @@ class MyApp extends StatelessWidget {
 }  
 
 class HomePage extends StatelessWidget {
+
+   final Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+    bearing: 192.8334901395799,
+    target: LatLng(37.43296265331129, -122.08832357078792),
+    tilt: 59.440717697143555,
+    zoom: 11.0);
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+  
    @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -44,16 +65,21 @@ class HomePage extends StatelessWidget {
         ),
         elevation: 0.0,
       ),
-      body: Center(
-        child: Text('Carte',
-              style: TextStyle(
-              fontFamily: 'Montserrat',
-              fontSize: 26,
-              fontWeight: FontWeight.bold,
-              color: Colors.black
-          )
-        ),
+      body: new Scaffold( 
+        body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        }
+       ),
+       floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
       ),
+     )
+      ,
       drawer: new Drawer(
         child: new ListView(
           children: <Widget>[
